b5ae20c4bfd82f4f8b148b9b9cac1f1da7b6d768
sukesh <sukeshkr8791@gmail.com>
2025-05-12 10:28:24 +0530

    unwanted package removed

--- src/main/java/com/tyss/movie_management_system/entity/Actor.java
+++ src/main/java/com/tyss/movie_management_system/entity/Actor.java
@@ -1,24 +1,5 @@
 package com.tyss.movie_management_system.entity;
 
-import java.util.List;
-
-import org.hibernate.annotations.GeneratorType;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 
 public class Actor {
 
--- src/main/java/com/tyss/movie_management_system/entity/Movie.java
+++ src/main/java/com/tyss/movie_management_system/entity/Movie.java
@@ -1,23 +1,5 @@
 package com.tyss.movie_management_system.entity;
 
-import java.util.List;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 public class Movie {
 
 }
