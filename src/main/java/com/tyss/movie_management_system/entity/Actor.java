7d81558d1d81163d5f70d8326e39673d5fdcb0b1
Nithin <icl17nithinkumars@gmail.com>
2025-05-12 11:33:46 +0530

    mapped movies with actor-OneToMany

--- src/main/java/com/tyss/movie_management_system/entity/Actor.java
+++ src/main/java/com/tyss/movie_management_system/entity/Actor.java
@@ -1,6 +1,21 @@
 package com.tyss.movie_management_system.entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "actors")
 public class Actor {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer id;
+private String name;
+private Integer dob;
 
 }
--- src/main/java/com/tyss/movie_management_system/entity/Movie.java
+++ src/main/java/com/tyss/movie_management_system/entity/Movie.java
@@ -1,5 +1,25 @@
 package com.tyss.movie_management_system.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movies")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer movieId;
+    private String movieName;
+    private String industry;
+
+    @OneToMany(mappedBy = "movies",cascade = CascadeType.ALL)
+    private List<Actor> actors;
 
 }
