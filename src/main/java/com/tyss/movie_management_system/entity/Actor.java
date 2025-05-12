205fc14378b3eb785985eac478820595857b1a04
Nithin <icl17nithinkumars@gmail.com>
2025-05-12 12:27:54 +0530

    Final Submission of modification

--- src/main/java/com/tyss/movie_management_system/entity/Actor.java
+++ src/main/java/com/tyss/movie_management_system/entity/Actor.java
@@ -1,6 +1,28 @@
 package com.tyss.movie_management_system.entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "actors")
 public class Actor {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Integer id;
+ private String name;
+ private Integer dob;
+
+ @ManyToOne
+ private Movie movies;
+
+
 
 }
