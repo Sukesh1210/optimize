6d7900fca07f3773d90e300e29c674f6cb4c7a54
Sukesh kumar <sukeshkr8791@gmail.com>
2025-08-01 14:00:47 +0530

    Revert "language added"
    This reverts commit 1885c9876c886962c124d0aadbc20664ecc5db9a.

--- src/main/java/com/tyss/movie_management_system/entity/Actor.java
+++ src/main/java/com/tyss/movie_management_system/entity/Actor.java
@@ -23,4 +23,6 @@ public class Actor {
  @ManyToOne
  private Movie movies;
 
+
+
 }
--- src/main/java/com/tyss/movie_management_system/entity/Language.java
+++
@@ -1,7 +0,0 @@
-package com.tyss.movie_management_system.entity;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Language {
-}
