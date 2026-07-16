d24e54497933427b2d1e936dec62af53460f842a
Dell User <sukeshkr8791@gmail.com>
2026-07-16 12:02:34 +0530

    New Entity created

---
+++ src/main/java/com/tyss/movie_management_system/entity/Language.java
@@ -0,0 +1,14 @@
+package com.tyss.movie_management_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer languageId;
+
+}
